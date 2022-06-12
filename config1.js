01311fac7e392d04771fe96d734d744224c81b59
binibabu <binibabu@gmail.com>
2022-06-12 15:10:32 +0530

    Nodejs Swagger

---
+++ config1.js
@@ -0,0 +1,15 @@
+const env = process.env;
+
+const config1 = {
+  db: { /* Password and below sensitive info are exposed for demo, PROD launch needs app config and Cloud SQL instances */
+    host: env.DB_HOST || 'localhost',
+    user: env.DB_USER || '${DB_USER}',
+    password: env.DB_PASSWORD || '${DB_PASSWORD}',
+    database: env.DB_NAME || 'titanic',
+  },
+
+  listPerPage: env.LIST_PER_PAGE || 10,
+ 
+};
+
+module.exports = config1;
